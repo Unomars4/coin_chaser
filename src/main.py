ec15dd5093822cbeeff49fc715bbb9c4a52651e1
Unomars4 <une.nyide@gmail.com>
2023-08-27 12:00:44 +0200

    add map & load images function

--- src/main.py
+++ src/main.py
@@ -1,13 +1,23 @@
 # Complete your game here
+import pygame
+
 class CoinChaser:
     def init(self):
-        pass
+        pygame.init()
 
     def load_images(self):
-        pass
+        self.images = []
+        for name in ["coin", "door", "monster", "robot"]:
+            self.images.append(pygame.image.load(name + ".png"))
 
     def new_game(self):
-        self.map = []
+        self.map = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
 
     def main_loop(self):
         pass
@@ -16,7 +26,9 @@ class CoinChaser:
         pass
 
     def draw_window(self):
-        pass
+        self.window.fill((255, 255, 255))
+
+        
 
 
 CoinChaser()
