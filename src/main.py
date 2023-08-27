60a61fae21e37070d52b9e6be66fde56e29c31c9
Unomars4 <une.nyide@gmail.com>
2023-08-27 12:13:42 +0200

    add main loop, check events and draw window

--- src/main.py
+++ src/main.py
@@ -5,6 +5,22 @@ class CoinChaser:
     def init(self):
         pygame.init()
 
+        self.load_images()
+        self.new_game()
+
+        self.height = len(self.map)
+        self.width = len(self.map[0])
+        self.scale = self.images[0].get_width()
+
+        window_height = self.scale * self.height
+        window_width = self.scale * self.width
+        self.window = pygame.display.set_mode((window_width, window_height))
+
+        pygame.display.set_caption("Coin Chaser")
+
+        self.main_loop()
+
+
     def load_images(self):
         self.images = []
         for name in ["coin", "door", "monster", "robot"]:
@@ -20,14 +36,24 @@ class CoinChaser:
                     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
 
     def main_loop(self):
-        pass
+        while True:
+            self.check_events()
+            self.draw_window()
 
     def check_events(self):
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
 
     def draw_window(self):
         self.window.fill((255, 255, 255))
-
+        
+        for y in range(self.height):
+            for x in range(self.width):
+                square = self.map[y][x]
+                self.window.blit(self.images[square], (x * self.scale, y * self.scale))
+        
+        pygame.display.flip()
         
 
 
